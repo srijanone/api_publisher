aab93eba2514d04bc92cc669f1e28394a9100193
Dileep Maurya <dileepmaurya.info@gmail.com>
2021-05-31 10:23:04 +0530

    validating service and routes

--- src/KongAPI/KongEntity/MapServiceAndRoute.php
+++ src/KongAPI/KongEntity/MapServiceAndRoute.php
@@ -2,32 +2,66 @@
 
 namespace Drupal\kong_api_publisher\KongAPI\KongEntity;
 
+/**
+ * Default implementaion of mapping OAS v2 to kong entity
+ * It's a default implementaion based on OAS v2 aka Swagger 2
+ */
 trait MapServiceAndRoute {
 
+  /**
+   * Mapping OAS2 spec to kong service and routes
+   */
   protected function mapServiceAndRoute() {
     $services = $this->mapService();
-    $routes = $this->mapRoute($services['name']);
-    $this->declartiveConfig['services'] = $services;
-    $this->declartiveConfig['routes'] = $routes;
+    $routes = $this->mapRoutes($services['name']);
+    $this->setServices($services);
+    $this->setRoutes($routes);
   }
 
-  protected function mapRoute($serviceName) {
+  /**
+   * Mapping OAS2 spec paths to kong routes
+   *
+   * @param String $serviceName service name to which routes belong
+   */
+  protected function mapRoutes($serviceName) {
     $routes = [];
     foreach ($this->oas['paths'] as $path => $items) {
       foreach ($items as $method => $route) {
-        $routes[] = [
-          'service' => ['name' => $serviceName],
-          'name' => $this->sanitizeName($route['operationId']),
-          'methods' => [strtoupper($method)],
-          'paths' => [$path],
-        ];
+        $routes[] = $this->mapRoute([
+          'service' => $serviceName,
+          'operationId' => $route['operationId'],
+          'path' => $path,
+          'method' => $method,
+        ]);
       }
     }
 
     return $routes;
   }
 
+  /**
+   * Mapping single paths to kong route
+   *
+   * @param Array $route associate route info
+   */
+  protected function mapRoute($route) {
+    $this->validateRoute($route);
+
+    return [
+      'service' => ['name' => $route['service']],
+      'name' => $this->sanitizeName($route['operationId']),
+      'methods' => [strtoupper($route['method'])],
+      'paths' => [$route['path']],
+    ];
+  }
+
+  /**
+   * Mapping OAS2 to kong service
+   *
+   */
   protected function mapService() {
+    $this->validateService();
+
     return [
       'name' => $this->sanitizeName($this->oas['info']['title']),
       'host' => $this->oas['host'],
@@ -35,9 +69,48 @@ trait MapServiceAndRoute {
     ];
   }
 
+  /**
+   * Utils function to sanitize text as per kong entity name attribute
+   *
+   * @param String $name
+   *
+   * @return String sanitized string
+   */
   protected function sanitizeName($name) {
     $name = explode(' ', $name);
 
     return implode('-', $name);
+  }
+
+  /**
+   * Validation check for kong route
+   *
+   * @param Array $route associative array
+   */
+  protected function validateRoute($route) {
+    $keysToCheck = ['service', 'operationId', 'method', 'path'];
+    foreach ($keysToCheck as $key) {
+      if (!isset($route[$key])) {
+        throw new \Exception('Please provide ' . $key . ' attributes in paths');
+      }
+    }
+  }
+
+  /**
+   * Validation check for kong service
+   *
+   */
+  protected function validateService() {
+    $keysToCheck = ['host', 'basePath'];
+
+    foreach ($keysToCheck as $key) {
+      if (!isset($this->oas[$key])) {
+        throw new \Exception('Please provide ' . $key . ' attributes in spec');
+      }
+    }
+
+    if (!isset($this->oas['info']['title'])) {
+      throw new \Exception('Please provide title attribute in spec');
+    }
   }
 }
--- src/KongAPI/KongEntity/OpenAPI3Kong.php
+++ src/KongAPI/KongEntity/OpenAPI3Kong.php
@@ -2,21 +2,49 @@
 
 namespace Drupal\kong_api_publisher\KongAPI\KongEntity;
 
+/**
+ * Generting kong entity based on
+ * OAS v3
+ */
 class OpenAPI3Kong extends OpenAPIKong {
 
   use MapServiceAndRoute;
 
+  /**
+   * Implement abstract method createDeclarativeConfig
+   * as per OAS v2
+   */
   protected function createDeclarativeConfig() {
     $this->mapServiceAndRoute();
   }
 
+  /**
+   * Mapping service as per OAS v3
+   */
   protected function mapService() {
+    $this->validateService();
+
     $url = $this->oas['servers'][0]['url'];
     $parsedUrl = parse_url($url);
 
     return [
       'name' => $this->sanitizeName($this->oas['info']['title']),
       'host' => $parsedUrl['host'],
+      'path' => $parsedUrl['path'],
     ];
+  }
+
+  /**
+   * Validating the parsed OAS spec before mapping to
+   * kong entity
+   */
+  protected function validateService() {
+    if (!isset($this->oas['info']['title'])) {
+      throw new \Exception('Please provide title attribute in spec');
+    }
+
+    if (!$this->oas['servers'][0]['url']) {
+      throw new \Exception('Please provide atleast one servers url');
+    }
   }
 }
